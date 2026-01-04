3ad69741fb8a34ac6203f739a042f22ddcd9de17
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2026-01-04 08:38:31 -0400

    REFACTOR

--- fhir-types/test/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import qualified FHIR.Types.Complex.AttachmentSpec as AttachmentSpec
-import Test.Hspec
-
-main :: IO ()
-main = hspec $ do
-    describe "FHIR.Types.Complex" $ do
-        AttachmentSpec.spec
